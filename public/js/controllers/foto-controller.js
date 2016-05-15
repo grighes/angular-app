fff57f4f2bd859934503ca09bd46d2df20b296d1
grighes <glauber.righes@gmail.com>
2016-05-15 13:19:32 -0300

    editando imagens

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -2,19 +2,44 @@ angular.module('angular').controller('FotoController', function($scope, $http, $
   $scope.foto = {};
   $scope.mensagem = '';
 
-  console.log($routeParams.fotoId);
+  /* Buscando foto no servidor pelo $routeParams */
+  if($routeParams.fotoId) {
+    $http.get('/v1/fotos/' + $routeParams.fotoId)
+    .success(function(foto) {
+      $scope.foto = foto;
+    })
+    .error(function(erro) {
+      console.log(erro);
+      $scope.mensagem = 'Não foi possível obter a foto'
+    });
+  }
 
   $scope.submeter = function() {
     if ($scope.formulario.$valid) {
-      $http.post('/v1/fotos', $scope.foto)
-      .success(function() {
-          $scope.foto = {};
-          $scope.mensagem = 'Foto cadastrada com sucesso';
-      })
-      .error(function(erro) {
+      
+      /* Se scope tem foto id então edita foto*/
+      if($scope.foto._id) {
+        $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+        .success(function() {
+          $scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso';
+        })
+        .error(function(erro) {
           console.log(erro);
-          $scope.mensagem = 'Não foi possível cadastrar a foto';
-      })
+          $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
+        });
+
+
+      } else {
+        $http.post('/v1/fotos', $scope.foto)
+        .success(function() {
+            $scope.foto = {};
+            $scope.mensagem = 'Foto cadastrada com sucesso';
+        })
+        .error(function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível cadastrar a foto';
+        })
+      }
     }
   };
   
